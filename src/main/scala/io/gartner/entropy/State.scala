bd254399886001e1a33960ff840cb565dcf922a2
Erik Gärtner <erik@gartner.io>
2017-02-26 19:24:08 +0100

    RDF class

--- src/main/scala/io/gartner/entropy/State.scala
+++ src/main/scala/io/gartner/entropy/State.scala
@@ -1,4 +1,6 @@
 package io.gartner.entropy
 
 abstract class State
-case class CharacterHasObject(character: Character, obj: Object) extends State
+class RelationTriple(subject: Any, relation: String, obj: Any) extends State
+
+case class CharacterHasObject(character: Character, obj: Object) extends RelationTriple(character, "has object", obj)
